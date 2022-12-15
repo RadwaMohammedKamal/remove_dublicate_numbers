36fde1a0632cb479f45c5cfc06bbe9740b4c7736
RadwaMohammedKamal <120174338+RadwaMohammedKamal@users.noreply.github.com>
2022-12-15 19:00:33 +0200

    Update list.dart

--- bin/list.dart
+++ bin/list.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 void main(List<String> arguments) {
   print('enter five numbers:');
-  Set  numbers={};  //let the user int  numbers to his list
+  Set  numbers={};  //let the user inter  numbers to his list
   for (int i=0;i<5;i++) {
     numbers.add(num.parse(stdin.readLineSync()!));
 
   }
+  //print numbers without dublicate
   numbers.forEach((element) {
     print('$element : hassan');
   });
